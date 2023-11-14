e4a86c7c3da42b7c8bbbff1fb02419d8902ccf2b
nrepoman <116200634+nrepoman@users.noreply.github.com>
2023-11-14 17:56:45 -0300

    Update wpace-main.js

--- wpace-main.js
+++ wpace-main.js
@@ -1,10 +1,1 @@
-/*
-    if (window.location.href === 'https://www.acesaojoao.com.br/portal/natal-magico/') {
-      const imagens = document.querySelectorAll('img');
-
-      imagens.forEach(imagem => {
-        imagem.classList.add('shk891');
-      });
-    }
-
-*/
+console.log('status... ok');
