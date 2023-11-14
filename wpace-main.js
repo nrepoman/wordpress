2577b05f8b5bc025654b1a4a48c4443e62bae351
nrepoman <116200634+nrepoman@users.noreply.github.com>
2023-11-14 10:10:42 -0300

    6 Update wpace-main.js

--- wpace-main.js
+++ wpace-main.js
@@ -7,31 +7,3 @@
       });
     }
 
-
-
-
-/* OLD SCRIPT
-
-function substituirTermo(element) {
-    if (element.nodeType === Node.TEXT_NODE) {
-        let texto = element.textContent;
-        let novoTexto = texto.replace(/Boa Vista/g, 'Broa Vista');
-        novoTexto = novoTexto.replace(/gratuito/g, 'gratuiro');
-        element.textContent = novoTexto;
-    } else if (element.nodeType === Node.ELEMENT_NODE) {
-        for (let i = 0; i < element.childNodes.length; i++) {
-            substituirTermo(element.childNodes[i]);
-        }
-    }
-}
-
-function substituirTermosNaPaginaEspecifica() {
-    if (window.location.href === 'https://www.acesaojoao.com.br/portal/natal-magico/') {
-        substituirTermo(document.body);
-    } else {
-        console.log('Not working in this page.');
-    }
-}
-
-substituirTermosNaPaginaEspecifica();
-*/
