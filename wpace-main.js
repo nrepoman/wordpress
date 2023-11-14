18ec45d5597d3ef3c2876352ef24b93732206379
nrepoman <116200634+nrepoman@users.noreply.github.com>
2023-11-14 09:21:26 -0300

    Update wpace-main.js

--- wpace-main.js
+++ wpace-main.js
@@ -1,1 +1,20 @@
-console.log('status... ok');
+function substituirTermo(element) {
+    if (element.nodeType === Node.TEXT_NODE) {
+        let texto = element.textContent;
+        let novoTexto = texto.replace(/Boa Vista/g, 'Broa Vista');
+        element.textContent = novoTexto;
+    } else if (element.nodeType === Node.ELEMENT_NODE) {
+        // Se for um elemento, chama recursivamente para seus filhos
+        for (let i = 0; i < element.childNodes.length; i++) {
+            substituirTermo(element.childNodes[i]);
+        }
+    }
+}
+
+
+function substituirTermoEmTodoDocumento() {
+    substituirTermo(document.body);
+}
+
+substituirTermoEmTodoDocumento();
+
